4cb8aa8526783673944f03738665f6979e5031a0
Alex Kibe <alex.kibe01@student.moringaschool.com>
2024-09-26 11:28:29 +0300

    Setup db

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,25 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+db = SQLAlchemy()
+
+def initialize_models():
+    pass
 
 def create_app(config_name):
     app.config.from_object(config_name)
+   
+    #Initialize DB with app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+   
+    # Create database schemas
+    with app.app_context():
+        initialize_models()
 
 
     return app
