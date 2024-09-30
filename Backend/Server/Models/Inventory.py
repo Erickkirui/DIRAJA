89f6a337a78cd48c0dd91ddd2ba1685b72328a6f
Alex Kibe <alex.kibe01@student.moringaschool.com>
2024-09-30 06:02:05 +0300

    Created Inventory table

---
+++ Backend/Server/Models/Inventory.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import db
+from sqlalchemy.orm import validates
+from sqlalchemy import func
+
+
+class Inventory(db.Model):
+    __tablename__= "inventory"
+
+    #Table columns
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quantity = db.Column (db.Float, nullable=False)
+    metric =db.Column (db.String)
+    unitCost = db.Column (db.Float, nullable=False)
+    totalCost = db.Column (db.Float, nullable=False)
+    amountPaid = db.Column (db.Float, nullable=False)
+    unitPrice = db.Column (db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    
+    
+    #validations
+    
+    @validates('metric')
+    def validate_metric(self, key,metric):
+        valid_metric = ['item', 'kg', 'ltrs']
+        assert metric in metric, f"Invalid metric. Must be one of: {', '.join(valid_metric)}"
+        return metric
+
+
+
+    def __repr__(self):
+        return f"Stock (id{self.id}, itemname='{self.item_name}', quantity='{self.quantity}', metric='{self.metric}', totalcost='{self.totalCost}', unitcost='{self.unitCost}', amountpaid='{self.amountPaid}', unitprice='{self.unitPrice}')"
