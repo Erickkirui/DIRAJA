8ba07fce9a19a074f2f378d23d43976c8cf8a961
erickkirui <erickkirui653@gmail.com>
2024-10-01 21:49:51 +0300

    removed extra column from shops table

--- Backend/Server/Views/Usersviews.py
+++ Backend/Server/Views/Usersviews.py
@@ -1,6 +1,7 @@
 from  flask_restful import Resource
 from Server.Models.Users import Users
 from app import db
+from flask import request
 
 
 
@@ -11,7 +12,21 @@ class CountUsers(Resource):
         return {"total users": countUsers}, 200
 
 class Addusers(Resource):
-    pass
+    def post (self):
+        data = request.get_json()
+
+        username = data.get('username')
+        email = data.get('email')
+        role = data.get('role')
+        password = data.get('password')
+
+        user = Users(username=username, email=email, password=password, role=role)
+        db.session.add(user)
+        db.session.commit()
+
+
+        return {'message': 'User added successfully'}, 201
+    
 
 
 class UsersResourceById(Resource):
