36d84a7ace6afebb8d2cb9174c7fc8ea6950bed9
Alex Kibe <alex.kibe01@student.moringaschool.com>
2024-10-02 12:19:45 +0300

    Created Shops endponts

--- Backend/Server/Views/__init__.py
+++ Backend/Server/Views/__init__.py
@@ -5,7 +5,7 @@ api_endpoint = Blueprint
 
 # add all file inputs 
 from Server.Views.Usersviews import CountUsers,Addusers,UsersResourceById
-
+from Server.Views.Shopsviews import AddShops, ShopsResourceById, ShopsResourceByName
 
 api_endpoint = Blueprint('auth',__name__,url_prefix='/diraja')
 api = Api(api_endpoint)
@@ -18,3 +18,7 @@ api.add_resource(CountUsers, '/countusers')
 api.add_resource(Addusers , '/newuser')
 api.add_resource(UsersResourceById, '/user/<int:users_id>')
 
+# shops endpoints 
+api.add_resource(AddShops, '/newshop')
+api.add_resource(ShopsResourceById, '/shop/<int:shops_id>')
+api.add_resource(ShopsResourceByName, '/shop/<string:shopname>')
