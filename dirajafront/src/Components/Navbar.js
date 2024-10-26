8d704d051917aa2c08ced4b13866117eb5b0ccc9
erickkirui <erickkirui653@gmail.com>
2024-10-26 08:58:21 +0300

    added active link to the nav menu

--- dirajafront/src/Components/Navbar.js
+++ dirajafront/src/Components/Navbar.js
@@ -1,63 +1,57 @@
-import React from 'react'
-import '../Styles/navigation.css'
-import { Link } from 'react-router-dom';
+import React from 'react';
+import '../Styles/navigation.css';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <>
     <div className='navigation-container'>
-        <h1>DIRAJA SHOP</h1>
+      <h1>DIRAJA SHOP</h1>
 
-        <div className='main-menu'>
-            <h4>MAIN MENU</h4>
+      <div className='main-menu'>
+        <h4>MAIN MENU</h4>
 
-            <ul>
-                <div className='menu-item'>
-                    <img src='/images/Dashbord.png' alt='menu-icon' />
-                    <li><Link to="/">Dashbord</Link></li>
-                </div>
-                <div className='menu-item'>
-                    <img src='/images/Sales.png' alt='menu-icon' />
-                    <li><Link to="/sales">Sales</Link></li>
-                </div>
-                <div className='menu-item'>
-                    <img src='/images/Sales.png' alt='menu-icon' />
-                    <li><Link to="/allinventory">Inventory</Link></li>
-                </div>
-                <div className='menu-item'>
-                    <img src='/images/Expenses.png' alt='menu-icon' />
-                    <li><Link to="/expenses">Expenses</Link></li>
-                </div>
-                <div className='menu-item'>
-                    <img src='/images/Shop.png' alt='menu-icon' />
-                    <li><Link to="/allshops">Shops</Link></li>
-                </div>
-                <div className='menu-item'>
-                    <img src='/images/Customers.png' alt='menu-icon' />
-                    <li><Link to="/allcustomers">Customers</Link></li>
-                </div>
-                <div className='menu-item'>
-                    <img src='/images/Employees.png' alt='menu-icon' />
-                    <li><Link to="/allemployees">Employees</Link></li>
-                </div>
-                
-            </ul>
-        </div>
+        <ul>
+          <NavLink exact to="/" className="menu-item" activeClassName="active">
+            <img src='/images/Dashbord.png' alt='menu-icon' />
+            <li>Dashboard</li>
+          </NavLink>
+          <NavLink to="/sales" className="menu-item" activeClassName="active">
+            <img src='/images/Sales.png' alt='menu-icon' />
+            <li>Sales</li>
+          </NavLink>
+          <NavLink to="/allinventory" className="menu-item" activeClassName="active">
+            <img src='/images/Sales.png' alt='menu-icon' />
+            <li>Inventory</li>
+          </NavLink>
+          <NavLink to="/expenses" className="menu-item" activeClassName="active">
+            <img src='/images/Expenses.png' alt='menu-icon' />
+            <li>Expenses</li>
+          </NavLink>
+          <NavLink to="/allshops" className="menu-item" activeClassName="active">
+            <img src='/images/Shop.png' alt='menu-icon' />
+            <li>Shops</li>
+          </NavLink>
+          <NavLink to="/allcustomers" className="menu-item" activeClassName="active">
+            <img src='/images/Customers.png' alt='menu-icon' />
+            <li>Customers</li>
+          </NavLink>
+          <NavLink to="/allemployees" className="menu-item" activeClassName="active">
+            <img src='/images/Employees.png' alt='menu-icon' />
+            <li>Employees</li>
+          </NavLink>
+        </ul>
+      </div>
 
-        <div className='accounting-menu'>
-            <h4>ACCOUNTING</h4>
-            <ul>
-                <li>Balance Sheet</li>
-                <li>Cash Flow</li>
-                <li>Profit and Loss</li>
-            </ul>
-        </div>
-
-        
-
+      <div className='accounting-menu'>
+        <h4>ACCOUNTING</h4>
+        <ul>
+          <li>Balance Sheet</li>
+          <li>Cash Flow</li>
+          <li>Profit and Loss</li>
+        </ul>
+      </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
