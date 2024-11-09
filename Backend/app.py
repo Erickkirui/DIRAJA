46c9b9188a6758c38ab7d13f914ce8069f4617d1
erickkirui <erickkirui653@gmail.com>
2024-11-09 12:03:01 +0300

    live test

--- Backend/app.py
+++ Backend/app.py
@@ -3,10 +3,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 db = SQLAlchemy()
 jwt = JWTManager()
+
 
 def initialize_models():
     from Server.Models.Users import Users
