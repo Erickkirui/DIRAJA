7b2dd8433a56232d253ca5a8cd43698b661f5791
Alex Kibe <alex.kibe01@student.moringaschool.com>
2025-05-16 22:12:57 +0300

    Account balance access

--- dirajafront/src/Pages/AccountBalances.js
+++ dirajafront/src/Pages/AccountBalances.js
@@ -9,7 +9,7 @@ function AccountBalances() {
 
   useEffect(() => {
     const username = localStorage.getItem('username');
-    if (username !== 'Leo' && username !== 'Namai') {
+    if (username !== 'Leo' && username !== 'Namai' && username !== 'External Auditor') {
       navigate('/'); // redirect to dashboard if unauthorized
     }
   }, [navigate]);
