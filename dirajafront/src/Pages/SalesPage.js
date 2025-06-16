73ec66362a5c343a9e7d02f8c6ffff383ffbb942
Alex Kibe <alex.kibe01@student.moringaschool.com>
2025-06-16 22:01:54 +0300

    New changes

--- dirajafront/src/Pages/SalesPage.js
+++ dirajafront/src/Pages/SalesPage.js
@@ -1,38 +1,43 @@
 import React, { useState } from 'react';
-// import CashSales from '../Components/ManagerDashbord/CashSales';
 import Sales from '../Components/Sales/GetSales';
 import { Link } from 'react-router-dom';
 
-
 function SalesPage() {
   const [activeTab, setActiveTab] = useState('allsales');
+
   return (
-      <div>
-        <h1>All Sales</h1>
-        <Link to="/sale-reports">
-  <button
-    style={{
-      backgroundColor: 'transparent', // No background color
-      padding: '10px', // Padding of 10px
-      border: '1px solid #ccc', // 1px solid border with color #ccc
-      display: 'flex', // Flex to align items
-      alignItems: 'center', // Center items vertically
-      borderRadius: '5px',
-      cursor: 'pointer', // Pointer cursor on hover
-    }}
-  >
-    Generate Excel Report 
-    <img 
-      src='/images/office365.png' 
-      alt="Export Icon"
-      style={{
-        width: '20px', // Width of 20px
-        height: '20px', // Height of 20px
-        marginLeft: '5px', // Margin to space the image from the text
-      }} 
-    />
-  </button>
-</Link>
+    <div>
+      <h1>All Sales</h1>
+
+      <div style={{ display: 'flex', gap: '10px', marginBottom: '20px' }}>
+        <Link to="/sale-reports" style={{ textDecoration: 'none' }}>
+          <button
+            style={{
+              backgroundColor: 'transparent',
+              padding: '10px',
+              border: '1px solid #ccc',
+              display: 'flex',
+              alignItems: 'center',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}
+          >
+            Generate Excel Report
+            <img
+              src="/images/office365.png"
+              alt="Export Icon"
+              style={{
+                width: '20px',
+                height: '20px',
+                marginLeft: '5px',
+              }}
+            />
+          </button>
+        </Link>
+
+        <Link className='add-button' to="/newsale">Add Sale </Link>
+        
+      </div>
 
         <Sales />
        
