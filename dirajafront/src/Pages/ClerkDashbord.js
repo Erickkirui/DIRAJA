f6f486f10128fc183a69e046f4a449e9416d2150
Erickkirui <erickkirui653@gmail.com>
2025-07-31 09:59:56 +0300

    added stock

--- dirajafront/src/Pages/ClerkDashbord.js
+++ dirajafront/src/Pages/ClerkDashbord.js
@@ -5,9 +5,25 @@ import Totalcreditpershop from '../Components/ClerkDashbord/Totalcreditpershop';
 import TotalCashSalesByUser from '../Components/ClerkDashbord/TotalCashSalesperuser';
 import TotalMabandaSales from '../Components/ClerkDashbord/MabandaShop/Totalsalesmabanda';
 import TotalShopSales from '../Components/ClerkDashbord/TotalSalesPershop';
+import ShopNameDisplay from '../Components/ClerkDashbord/ShopNameDisplay';
 import '../Styles/clerkpage.css';
 import { Link } from 'react-router-dom';
-import ShopNameDisplay from '../Components/ClerkDashbord/ShopNameDisplay';
+
+// Icons
+import {
+  FaWarehouse,
+  FaMoneyBillWave,
+  FaCashRegister,
+  FaEye,
+  FaCreditCard,
+  FaTrashAlt,
+  FaPiggyBank,
+  FaFileInvoiceDollar,
+  FaTruck,
+  FaTools,
+   FaBoxes
+} from 'react-icons/fa';
+import ShopStockList from '../Components/ClerkDashbord/ShopStockList';
 
 function ClerkDashbord() {
   const designation = localStorage.getItem('designation');
@@ -22,102 +38,137 @@ function ClerkDashbord() {
         <div className='body-header'>
           <UserDisplay />
         </div>
+        
 
         <div className='page-area'>
           <div className='nav-phone'>
             <ShopNameDisplay />
-            
-            {designation === "procurement" && (
-              <Link className='clerk-button' to='/distribute-stock'>Distribute Stock</Link>
-            )}
 
-            {shopId === "12" && (
+            {/* NEW SALE BUTTON */}
+            {shopId === '12' && (
               <Link className="clerk-button" to="/mabandasale">
                 New Sale
               </Link>
             )}
-
-            {shopId !== "12" && shopId !== "18" && (
+            {shopId !== '12' && shopId !== '18' && (
               <Link className="clerk-button" to="/shopsale">
                 New Sale
               </Link>
             )}
-
-            {shopId === "18" && (
+            {shopId === '18' && (
               <Link className="clerk-button" to="/promo-sale">
                 New Sale
               </Link>
             )}
 
-            {shopId === "2" && (
-              <Link className='clerk-button' to='/managestock'>Manage Stock</Link>
-            )}
+            <div className='icon-container'>
 
-            {shopId === "12" && (
-              <>
-                <Link className="clerk-button" to="/mabandastock">
-                  Add Stock
-                </Link>
-                <Link className="clerk-button" to="/mabandaexpense">
-                  Add Expense
-                </Link>
-                <Link className="clerk-button" to="/mabandapurchase">
-                  Add Purchase
-                </Link>
-              </>
-            )}
+              {/* Procurement Group */}
+              {designation === "procurement" && (
+                <div className='icon-group'>
+                  <Link className='clerk-icon-button' to='/distribute-stock'>
+                    <FaTruck className="icon" />
+                    <span>Distribute Stock</span>
+                  </Link>
+                  {/* Placeholder or another procurement icon could go here */}
+                </div>
+              )}
 
-            {designation === "reliever" && (
-              <Link className='clerk-button' to='/relieversales'>Reliever Sales</Link>
-            )}
+              {/* Shop 2 Group */}
+              {shopId === '2' && (
+                <div className='icon-group'>
+                 
+                   <Link className='clerk-icon-button' to='/shop-stock-level'>
+                    <FaBoxes className="icon" />
+                    <span>Stock</span>
+                  </Link>
 
-            {shopId === "12" && (
-              <Link className="clerk-button" to="/mabandasales">
-                View Sales
-              </Link>
-            )}
+                  <Link className='clerk-icon-button' to='/managestock'>
+                    <FaTools className="icon" />
+                    <span>Manage Stock</span>
+                  </Link>
+                </div>
+              )}
 
-            {shopId !== "12" && shopId !== "18" && (
-              <Link className="clerk-button" to="/shopsales">
-                View Sales
-              </Link>
-            )}
+              {/* Mabanda Group */}
+              {shopId === '12' && (
+                <>
+                  <div className='icon-group'>
+                    <Link className="clerk-icon-button" to="/mabandastock">
+                      <FaWarehouse className="icon" />
+                      <span>Add Stock</span>
+                    </Link>
+                    <Link className="clerk-icon-button" to="/mabandaexpense">
+                      <FaFileInvoiceDollar className="icon" />
+                      <span>Add Expense</span>
+                    </Link>
+                  </div>
+                  <div className='icon-group'>
+                    <Link className="clerk-icon-button" to="/mabandapurchase">
+                      <FaPiggyBank className="icon" />
+                      <span>Add Purchase</span>
+                    </Link>
+                    <Link className="clerk-icon-button" to="/mabandasales">
+                      <FaEye className="icon" />
+                      <span>View Sales</span>
+                    </Link>
+                  </div>
+                </>
+              )}
 
-            {shopId !== "12" && shopId !== "18" && (
-              <Link className="clerk-button" to="/shopcredit">
-                View Credit Sales
-              </Link>
-            )}
+              {/* Reliever Group */}
+              {designation === "reliever" && (
+                <div className='icon-group'>
+                  <Link className='clerk-icon-button' to='/relieversales'>
+                    <FaCashRegister className="icon" />
+                    <span>Reliever Sales</span>
+                  </Link>
+                </div>
+              )}
 
-            {shopId !== "12" && shopId !== "18" && (
-              <Link className="clerk-button" to="/addspoiltstock">
-                Spoilt Stock
-              </Link>
-            )}
-
-            {/* Updated deposit cash link based on designation */}
-            {shopId !== "12" && shopId !== "18" && (
-              designation === "reliever" ? (
-                <Link className="clerk-button" to="/deposit">
-                  Deposit Cash
-                </Link>
-              ) : (
-                <Link className="clerk-button" to="/depositcash">
-                  Deposit Cash
-                </Link>
-              )
-            )}
+              {/* Regular Shop Clerk Group */}
+              {shopId !== '12' && shopId !== '18' && (
+                <>
+                  <div className='icon-group'>
+                    <Link className="clerk-icon-button" to="/shopsales">
+                      <FaEye className="icon" />
+                      <span>View Sales</span>
+                    </Link>
+                    <Link className="clerk-icon-button" to="/shopcredit">
+                      <FaCreditCard className="icon" />
+                      <span>Credit Sales</span>
+                    </Link>
+                  </div>
+                  <div className='icon-group'>
+                    <Link className="clerk-icon-button" to="/addspoiltstock">
+                      <FaTrashAlt className="icon" />
+                      <span>Spoilt Stock</span>
+                    </Link>
+                    {designation === 'reliever' ? (
+                      <Link className="clerk-icon-button" to="/deposit">
+                        <FaMoneyBillWave className="icon" />
+                        <span>Deposit Cash</span>
+                      </Link>
+                    ) : (
+                      <Link className="clerk-icon-button" to="/depositcash">
+                        <FaMoneyBillWave className="icon" />
+                        <span>Deposit Cash</span>
+                      </Link>
+                    )}
+                  </div>
+                </>
+              )}
+            </div>
           </div>
 
+          {/* Dashboard Metrics */}
           <div className="metrix-pair">
             <div className='analytics-clerk'>
-              {shopId === "12" ? <TotalMabandaSales /> : <TotalShopSales />}
+              {shopId === '12' ? <TotalMabandaSales /> : <TotalShopSales />}
             </div>
-
             <div className='analytics-clerk'>
               <Totalcreditpershop />
             </div>
-            
             <div className='analytics-clerk'>
               <TotalCashSalesByUser />
             </div>
